afd603685dddad9752815f963f682e6353a87025
Álef Luciano <alefluciano28@gmail.com>
2021-01-24 17:54:04 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,44 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Seridó                     ///
+///               Departamento de Computação e Tecnologia                   ///
+///                  Disciplina DCT1106 -- Programação                      ///
+///                  Projeto Sistema de Gestão Escolar                      ///
+///                Developed by  @lefLuciano - Jan, 2021                    ///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void menuSobre(void);
+
+int main(void) {
+    menuSobre();
+    return 0;
+
+}
+
+
+void menuSobre(void) {
+    printf("\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("|                                                                             |\n");
+    printf("|               Universidade Federal do Rio Grande do Norte                   |\n");
+    printf("|                   Centro de Ensino Superior do Seridó                       |\n");
+    printf("|                 Departamento de Computação e Tecnologia                     |\n");
+    printf("|                    Disciplina DCT1106 -- Programação                        |\n");
+    printf("|                    Projeto Sistema de Planejamento de Dieta                 |\n");
+    printf("|                  Developed by  @lefLuciano - Jan, 2021                      |\n");
+    printf("|                                                                             |\n");
+    printf("-------------------------------------------------------------------------------\n\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("|                                                                             |\n");
+    printf("|            = = = = = Sistema de Planejamento de Dieta = = = = =             |\n");
+    printf("|                                                                             |\n");
+    printf("|  Programa utilizado para a disciplina DCT1106 - Programação, consite em     |\n");
+    printf("|  em um projeto que propõe um sistema capaz de auxiliar o(a) nutricionista   |\n");
+    printf("|  na sugestão de cardápios, alternando os tipos de alimentos a serem         |\n");
+    printf("|  consumidos, de forma que os cardápios da dieta não se tornem repetitivos   |\n");
+    printf("|                                                                             |\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("\n");
+}
