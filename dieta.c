6b6b1af0e9a198d04659424822cf22244e95e3e6
Álef Luciano <>
2021-05-04 04:22:48 +0000

    deita

--- dieta.c
+++ dieta.c
@@ -29,9 +29,13 @@ void moduloDieta(void) {
 }
 
 void cadastrarDieta(void) {
-	// função ainda em desenvolvimento
-	// exibe a tela apenas para testes
-	telaCadastrarDieta();
+	Dieta *diet;
+
+	diet = telaCadastrarDieta(); 
+
+  gravarDieta(diet);
+
+  free(diet);
 }
 
 void pesquisarDieta(void) {
@@ -86,9 +90,11 @@ char menuDieta(void) {
 
 }
 
-void telaCadastrarDieta(void) {
-	char codDieta[4];
-  char focoDaDieta[20];
+// Dieta* telaCadastrarDieta(void)
+
+Dieta* telaCadastrarDieta(void) {
+  Dieta * diet;
+	diet = (Dieta*) malloc ( sizeof (Dieta));
 
   system("clear");
 	printf("\n");
@@ -99,16 +105,36 @@ void telaCadastrarDieta(void) {
   printf("|     ===================================================================     |\n");
   printf("|                                                                             |\n");
   printf("|     Escreva um codigo para a dieta: ");
-	scanf("%[0-9]", codDieta);
+	scanf("%[0-9]", diet->codDieta);
 	getchar();
-  printf("|     Escreva o foco da dieta: ");
-	scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", focoDaDieta);
+  printf("|     Escreva o objetivo da dieta(EX: perder peso: ");
+	scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", diet->objetivo);
 	getchar();
+  printf("|     Escreva o cafe da manhã da dieta: ");
+	scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ 0-9]", diet->cafeDaManhã );
+  getchar();
+  printf("|     Escreva o lanche da manhã da dieta: ");
+	scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ 0-9 ]", diet->lanche1 );
+  getchar();  
+  printf("|     Escreva o almoço da dieta: ");
+	scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ 0-9 ]", diet->almoco );
+  getchar();
+  printf("|     Escreva o lanche da tarde da dieta: ");
+	scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ 0-9 ]", diet->lanche2 );
+  getchar();
+  printf("|     Escreva o jantar da dieta: ");
+	scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ 0-9 ]", diet->jantar );
+  getchar();
+  printf("|     Escreva a ceia da dieta: ");
+	scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ 0-9 ]", diet->ceia );
+  getchar();
+  diet->status = 1;
   printf("|                                                                             |\n");
   printf("|                                                                             |\n");
   printf("-------------------------------------------------------------------------------\n");
   printf("\n");
 	delay(1);
+  return diet;
 }
 
 void telaPesquisarDieta(void) {
@@ -122,7 +148,7 @@ void telaPesquisarDieta(void) {
   printf("|     ==============            PESQUISAR DIETA            ==============     |\n");
   printf("|     ===================================================================     |\n");
   printf("|                                                                             |\n");
-  printf("|     Escreva o foco da dieta: ");
+  printf("|     Escreva o codigo da dieta: ");
 	scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", focoDaDieta);
 	getchar();
 printf("|                                                                             |\n");
@@ -175,3 +201,14 @@ void telaExcluirDieta(void) {
 	delay(1);
 }
 
+void gravarDieta(Dieta* diet) {
+  FILE* fp;
+  fp = fopen("dietas.dat", "ab");
+  if (fp == NULL) {
+    printf("Ops! Ocorreu um erro na abertura do arquivo!\n");
+    printf("Não é possível continuar este programa...\n");
+    exit(1);
+  }
+  fwrite(diet, sizeof(Dieta), 1, fp);
+  fclose(fp);
+}
--- dieta.h
+++ dieta.h
@@ -1,11 +1,28 @@
+typedef struct dieta Dieta;
+struct dieta {
+  char codDieta[4];
+  char objetivo[12];
+  char cafeDaManhã[7];
+  char lanche1[7];
+  char almoco[7];
+  char lanche2[7];
+  char jantar[7];
+  char ceia[7];
+  int status;
+};
+
 void moduloDieta(void);
 char menuDieta(void);
-void telaCadastrarDieta(void);
-void telaPesquisarDieta(void);
-void telaAtualizarDieta(void);
-void telaExcluirDieta(void);
+
 void cadastrarDieta(void);
 void pesquisarDieta(void);
 void atualizarDieta(void);
 void excluirDieta(void);
+
+Dieta* telaCadastrarDieta(void);
+void telaPesquisarDieta(void);
+void telaAtualizarDieta(void);
+void telaExcluirDieta(void);
+
+void gravarDieta(Dieta*);
 
